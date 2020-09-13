ceb7d23c8aad9502ab6b57aee9b2c9ae81ca3237
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-09-13 13:10:10 +0100

    add color-context

--- src/components/Anonymous.js
+++ src/components/Anonymous.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { useCount } from "../contexts/count-context";
+import { useColor } from "../contexts/color-context";
 
 function Anonymous() {
   const [count, setCount] = useCount();
+  const [color, setColor] = useColor();
+  const changeColor = () => {
+    if (color === "teal") {
+      return setColor("green");
+    }
+    return setColor("teal");
+  };
   return (
     <div>
-      <h4>Anonymous</h4>
+      <h4 style={{ color }}>Anonymous</h4>
       <p>
         <strong>Count:</strong> {count}
       </p>
       <button onClick={() => setCount((c) => c - 2)}> -2 </button>
+      <button
+        onClick={changeColor}
+        style={{ backgroundColor: color, color: "white" }}
+      >
+        Toggle Color
+      </button>
     </div>
   );
 }
